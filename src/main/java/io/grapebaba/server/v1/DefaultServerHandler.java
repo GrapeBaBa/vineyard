a2c102bbdd44829cf5b4956e50e161c9ef9ab90b
kaichen <281165273@qq.com>
2015-07-27 17:58:59 +0800

    add license

--- src/main/java/io/grapebaba/server/v1/DefaultServerHandler.java
+++ src/main/java/io/grapebaba/server/v1/DefaultServerHandler.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2015 281165273grape@gmail.com
+ *
+ * Licensed under the Apache License, version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.grapebaba.server.v1;
 
 import com.google.common.collect.Maps;
@@ -6,7 +20,6 @@ import com.google.common.reflect.ClassPath;
 import io.grapebaba.annotation.ServerHandlerProvider;
 import io.grapebaba.annotation.Service;
 import io.grapebaba.annotation.ServiceInterface;
-import io.grapebaba.config.ServerConfiguration;
 import io.grapebaba.protocol.MessageType;
 import io.grapebaba.protocol.v1.DefaultProtocolV1Header;
 import io.grapebaba.protocol.v1.ProtocolMessage;
