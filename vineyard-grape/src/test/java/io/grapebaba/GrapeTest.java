f520b00c362574c179ecf7b58bd99c3db7abb3a8
kaichen <281165273@qq.com>
2016-02-06 11:34:27 +0800

    fix time

--- vineyard-grape/src/test/java/io/grapebaba/GrapeTest.java
+++ vineyard-grape/src/test/java/io/grapebaba/GrapeTest.java
@@ -51,7 +51,7 @@ public class GrapeTest {
         final int port = 8076;
         final int timeout = 200;
         final int opaque = next();
-        final long waitingTime = 2000L;
+        final long waitingTime = 30L;
         final AtomicReference<Object> result = new AtomicReference<>();
         final CountDownLatch latch = new CountDownLatch(1);
         TcpServer server = Grape.serve(new InetSocketAddress(port),
@@ -85,7 +85,7 @@ public class GrapeTest {
         final int port = 8077;
         final int timeout = 200;
         final int opaque = next();
-        final long waitingTime = 10L;
+        final long waitingTime = 30L;
         final AtomicReference<Object> result = new AtomicReference<>();
         final CountDownLatch latch = new CountDownLatch(1);
         TcpServer server = Grape.serve(new InetSocketAddress(port),
@@ -119,7 +119,7 @@ public class GrapeTest {
         final int port = 8078;
         final int timeout = 200;
         final int opaque = next();
-        final long waitingTime = 10L;
+        final long waitingTime = 30L;
         final AtomicReference<Object> result = new AtomicReference<>();
         final CountDownLatch latch = new CountDownLatch(1);
         TcpServer server = Grape.serve(new InetSocketAddress(port),
